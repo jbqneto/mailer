4c3f3999c5f48878db95e27a07c03abff30f0bc4
José Neto <jbqneto@gmail.com>
2026-09-03 12:39:53 +0100

    refactor: remove smtp settings from projects

--- src/domain/project.ts
+++ src/domain/project.ts
@@ -1,21 +1,7 @@
-export interface SmtpConfig {
-  host: string;
-  port: number;
-  secure: boolean;
-  auth:
-    | false
-    | {
-        user: string;
-        password: string;
-      };
-}
-
 export interface ProjectConfig {
   id: string;
   apiKey: string;
-  fromEmail: string;
   fromName: string;
   replyTo?: string;
-  smtp: SmtpConfig;
   allowedTemplates: readonly string[];
 }
